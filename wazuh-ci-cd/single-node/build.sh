e9646cf06efbf5417f20e9dd53f361ae0ad0e113
root <root@WS-UY-PC001.whitestack.com>
2025-04-13 15:46:42 -0600

    100

---
+++ wazuh-ci-cd/single-node/build.sh
@@ -0,0 +1,9 @@
+docker-compose build  # Construye las imágenes locales
+docker tag single-node_wazuh.manager ghcr.io/syscon3/wazuh-manager:latest
+docker tag single-node_wazuh.indexer ghcr.io/syscon3/wazuh-indexer:latest
+docker tag single-node_wazuh.dashboard ghcr.io/syscon3/wazuh-dashboard:latest
+
+docker push ghcr.io/syscon3/wazuh-manager:latest
+docker push ghcr.io/syscon3/wazuh-indexer:latest
+docker push ghcr.io/syscon/wazuh-dashboard:latest
+
