7518d9a51392d3f8d05690b3ba9703ee6c734265
root <root@WS-UY-PC001.whitestack.com>
2025-04-14 01:04:50 -0600

    PP5

--- wazuh-ci-cd/single-node/build.sh
+++ wazuh-ci-cd/single-node/build.sh
@@ -3,25 +3,19 @@ COMMIT_HASH=$(git rev-parse --short HEAD)
 echo COMMIT_HASH: $COMMIT_HASH
 
 # 2. Construir con docker-compose
-docker-compose build
+docker-compose build --no-cache --pull
 
 # 3. Etiquetar con commit-hash
-docker tag single-node_wazuh.manager ghcr.io/syscon3/wazuh-manager:$COMMIT_HASH
-docker tag single-node_wazuh.indexer ghcr.io/syscon3/wazuh-indexer:$COMMIT_HASH
-docker tag single-node_wazuh.dashboard ghcr.io/syscon3/wazuh-dashboard:$COMMIT_HASH
+docker image tag ghcr.io/syscon3/wazuh-manager:latest  ghcr.io/syscon3/wazuh-manager:$COMMIT_HASH
+docker image tag single-node_wazuh.indexer ghcr.io/syscon3/wazuh-indexer:$COMMIT_HASH
+docker image tag single-node_wazuh.dashboard ghcr.io/syscon3/wazuh-dashboard:$COMMIT_HASH
 
 # 4. Push con commit-hash
 docker push ghcr.io/syscon3/wazuh-manager:$COMMIT_HASH
 docker push ghcr.io/syscon3/wazuh-indexer:$COMMIT_HASH
 docker push ghcr.io/syscon3/wazuh-dashboard:$COMMIT_HASH
 
-# 5. Tag de 'latest'
-docker tag mi-imagen:$COMMIT_HASH ghcr.io/tu-org/mi-imagen:latest
-docker tag ghcr.io/syscon3/wazuh-manager:$COMMIT_HASH ghcr.io/syscon3/wazuh-manager:latest
-docker tag ghcr.io/syscon3/wazuh-indexer:$COMMIT_HASH ghcr.io/syscon3/wazuh-indexer:latest
-docker tag ghcr.io/syscon3/wazuh-dashboard:$COMMIT_HASH ghcr.io/syscon3/wazuh-dashboard:latest
-
-# 6. Push con latest
+# 5. Push con latest
 docker push ghcr.io/syscon3/wazuh-manager:latest
 docker push ghcr.io/syscon3/wazuh-indexer:latest
 docker push ghcr.io/syscon3/wazuh-dashboard:latest
